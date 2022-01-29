cf655fb977861aad19c9bea653af435bc5ad12d3
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-29 17:27:06 +0900

    getting offset pixels

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 100; // 100px
+
 
 function shadow(e) {
-const width = hero.offsetWidth;
-const height = hero.offsetHeight;
-let { offsetX: x, offsetY: y } = e;
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+  let { offsetX: x, offsetY: y } = e;
 
-console.log(x, y);
-}
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+  console.log(xWalk, yWalk);
+
+};
+
+// console.log(x, y);
 
 hero.addEventListener('mousemove', shadow);
