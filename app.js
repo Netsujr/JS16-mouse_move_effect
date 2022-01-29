eeb8512ac32968731c04ab5447efbef86070d088
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-29 17:37:44 +0900

    shadows work nicely
    some math involved, not tooooo complicated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const walk = 25; // 100px
+const walk = 150; //px
 
 
 function shadow(e) {
@@ -16,7 +16,12 @@ function shadow(e) {
   const xWalk = Math.round((x / width * walk) - (walk / 2));
   const yWalk = Math.round((y / height * walk) - (walk / 2));
 
-  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,0,0,0.5)`;
+  text.style.textShadow = `
+  ${xWalk}px ${yWalk}px 0 rgba(255,188,188,1),
+  ${xWalk * -1}px ${yWalk}px 0 rgba(255,255,188,1),
+  ${yWalk * -1}px ${xWalk}px 0 rgba(175,170,255,1)
+
+  `;
 
 };
 
